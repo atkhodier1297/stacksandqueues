b649bad783ffd11cf6e7c62b5fe562214720911f
Adam Khodier <khodieradam1297@gmail.com>
2023-10-27 23:43:13 -0400

    stack queue example

---
+++ index.js
@@ -0,0 +1,60 @@
+// stack
+
+class Stack {
+    constructor() {
+      this.items = [];
+    }
+  
+    push(element) {
+      this.items.push(element);
+    }
+  
+    pop() {
+      if (!this.isEmpty()) {
+        return this.items.pop();
+      }
+    }
+  
+    peek() {
+      return this.items[this.items.length - 1];
+    }
+  
+    isEmpty() {
+      return this.items.length === 0;
+    }
+  
+    size() {
+      return this.items.length;
+    }
+  }
+
+  // queue
+
+  class Queue {
+    constructor() {
+      this.items = [];
+    }
+  
+    enqueue(element) {
+      this.items.push(element);
+    }
+  
+    dequeue() {
+      if (!this.isEmpty()) {
+        return this.items.shift();
+      }
+    }
+  
+    front() {
+      return this.items[0];
+    }
+  
+    isEmpty() {
+      return this.items.length === 0;
+    }
+  
+    size() {
+      return this.items.length;
+    }
+  }
+  
